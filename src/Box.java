cf224f5c62e2c396b03102130e925b7e1eead49f
zerozzz1989 <109763198+zerozzz1989@users.noreply.github.com>
2022-08-09 21:52:36 +0300

    Lesson4 Task1,2

---
+++ src/Box.java
@@ -0,0 +1,17 @@
+public class Box {
+    /*
+    public int weight = 50;
+    public int age = 33;
+    public String firstName = "Anna";
+    public String lastName = "Ale";
+    public int height = 180;
+*/
+
+        public int weight =50;
+        public int age =33;
+        public String firstName ="Anna";
+        public String lastName ="Ale";
+        public int height =180;
+
+
+}
